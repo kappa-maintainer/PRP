9cdfaa2aeb518ca6ab83d9ecbf8643a43a1488b3
kappa_maintainer <gkappa@outlands.top>
2022-06-02 22:40:59 +0800

    Hot-reload

--- src/main/java/gkappa/prp/mixin/MixinBookContentClasspathLoader.java
+++ src/main/java/gkappa/prp/mixin/MixinBookContentClasspathLoader.java
@@ -1,5 +1,7 @@
 package gkappa.prp.mixin;
 
+import com.google.common.base.Preconditions;
+import com.google.common.base.Stopwatch;
 import com.google.gson.JsonElement;
 import gkappa.prp.PRP;
 import net.minecraft.client.Minecraft;
@@ -8,18 +10,50 @@ import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import vazkii.patchouli.api.PatchouliAPI;
 import vazkii.patchouli.client.book.BookContentClasspathLoader;
 import vazkii.patchouli.client.book.BookContentLoader;
+import vazkii.patchouli.client.book.BookContentsBuilder;
 import vazkii.patchouli.common.book.Book;
+import vazkii.patchouli.common.book.BookRegistry;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.Collection;
+import java.util.List;
 
 
-@Mixin(BookContentClasspathLoader.class)
+@Mixin(value = BookContentClasspathLoader.class, remap = false)
 public class MixinBookContentClasspathLoader {
+
+	@Inject(at = @At("HEAD"), method = "findFiles")
+	private void findFiles(Book book, String dir, List<ResourceLocation> list, CallbackInfo ci) {
+		String prefix = String.format("%s/%s/%s/%s", BookRegistry.BOOKS_LOCATION, book.id.getPath(), BookContentsBuilder.DEFAULT_LANG, dir);
+		Collection<ResourceLocation> files = Minecraft.getInstance().getResourceManager().listResources(prefix, p -> p.endsWith(".json"));
+
+		files.stream()
+				.distinct()
+				.filter(file -> file.getNamespace().equals(book.id.getNamespace()))
+				.map(file -> {
+					// caller expects list to contain logical id's, not file paths.
+					// we end up going from path -> id -> back to path, but it's okay as a transitional measure
+					Preconditions.checkArgument(file.getPath().startsWith(prefix));
+					Preconditions.checkArgument(file.getPath().endsWith(".json"));
+					String newPath = file.getPath().substring(prefix.length(), file.getPath().length() - ".json".length());
+					// Vanilla expects `prefix` above to not have a trailing slash, so we
+					// have to remove it ourselves from the path
+					if (newPath.startsWith("/")) {
+						newPath = newPath.substring(1);
+					}
+					return new ResourceLocation(file.getNamespace(), newPath);
+				})
+				.forEach(list::add);
+
+	}
+
 	@Inject(at = @At("HEAD"), method = "loadJson", cancellable = true, remap = false)
 	private void loadJson(Book book, ResourceLocation resloc, @Nullable ResourceLocation fallback, CallbackInfoReturnable<JsonElement> callback) {
 		PRP.LOGGER.debug("Loading {}", resloc);
